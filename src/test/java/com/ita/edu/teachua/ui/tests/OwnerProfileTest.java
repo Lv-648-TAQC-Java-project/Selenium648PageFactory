cd7187e46236e9844ef9f5fcf23220d671d65bd1
Pidstavskiy <vitalii.pidstavskiy@gmail.com>
2021-12-07 20:44:55 +0200

    add issue annotation for TUA-159

--- src/test/java/com/ita/edu/teachua/ui/tests/OwnerProfileTest.java
+++ src/test/java/com/ita/edu/teachua/ui/tests/OwnerProfileTest.java
@@ -284,6 +284,7 @@ public class OwnerProfileTest extends TestRunner {
     }
 
     @Description("TUA-159: verify that a user can add location to the list of locations after filling in only mandatory fields with valid data")
+    @Issue("TUA-159")
     @Test(dataProvider = "verifyThatOwnerCanAddLocationToTheListOfLocationsWithValidDataDataProvider")
     public void addLocationWithValidDataOfMandatoryFields(String name, String town, String address, String coordinates, String phoneNumber) {
         boolean actualResult = new HeaderPage(driver).authorize(valueProvider.getAdminEmail(), valueProvider.getAdminPassword())
