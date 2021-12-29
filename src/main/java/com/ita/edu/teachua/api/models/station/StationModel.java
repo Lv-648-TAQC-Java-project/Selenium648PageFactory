3a869c0a434e9e0332f8a7858885b832505c7608
Andriy Barskyi <barskyiandriy@gmail.com>
2021-12-29 10:16:29 +0200

    fix models

--- src/main/java/com/ita/edu/teachua/api/models/station/StationModel.java
+++ src/main/java/com/ita/edu/teachua/api/models/station/StationModel.java
@@ -1,8 +1,8 @@
 package com.ita.edu.teachua.api.models.station;
 
 public class StationModel {
-    public Integer id;
-    public String name;
-    public String cityName;
-    public String districtName;
+    private Integer id;
+    private String name;
+    private String cityName;
+    private String districtName;
 }
