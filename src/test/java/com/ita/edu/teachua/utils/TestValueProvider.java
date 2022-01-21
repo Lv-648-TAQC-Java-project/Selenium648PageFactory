3ebdc3725f925189a2809561bd38d99ac6b2bf73
Andriy <krol17aq@gmail.com>
2022-01-21 14:39:40 +0200

    fix District

--- src/test/java/com/ita/edu/teachua/utils/TestValueProvider.java
+++ src/test/java/com/ita/edu/teachua/utils/TestValueProvider.java
@@ -41,5 +41,6 @@ public class TestValueProvider {
     public boolean getHeadlessMode() {
         return  properties != null ? Boolean.parseBoolean(properties.getProperty("HeadlessMode")) : Boolean.parseBoolean(System.getenv("TEST_TEACH_UA_HEADLESS_MODE"));
     }
+
 }
 //
